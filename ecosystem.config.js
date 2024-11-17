3ac916db7224e563c974d3e0ad10468b2abd335f
James10192 <djedjelipatrick@gmail.com>
2024-11-17 11:43:42 +0000

    To  start v4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,8 +7,15 @@ module.exports = {
       PORT: 3001
     },
     instances: 1,
+    exec_mode: "cluster",
     autorestart: true,
     watch: false,
-    max_memory_restart: '1G'
+    max_memory_restart: '1G',
+    env_production: {
+      NODE_ENV: "production"
+    },
+    error_file: "/var/log/pm2/datalys-app-error.log",
+    out_file: "/var/log/pm2/datalys-app-out.log",
+    time: true
   }]
 } 
